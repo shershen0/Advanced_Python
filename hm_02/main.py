c20e3ee4159b8605fd8a3cb96bf4415846560252
Andrey Sheremeev <sheremeev.andrey@gmail.com>
2022-02-16 23:39:48 +0300

    hm_02

---
+++ hm_02/main.py
@@ -0,0 +1,56 @@
+import os
+# import fibonacci
+# from fibonacci import calc_fibonacci
+# from PIL import Image
+from ExampleGeneratorOfAST import generate_image
+from ExampleGeneratorOfAST import fib
+
+def makeList(n):
+    lst = [[i for i in range(n)] for j in range(1, 2*n)]
+    print(lst)
+    return lst
+
+def easy(list):
+    begin = "\\begin{tabular} { "
+    m = max(map(len, list))
+    begin += "| c " * m + "| }"
+    begin += "\n  \hline\n"
+    end = "\n  \\hline\n\\end{tabular}\n"
+    table = "".join(map(lambda x: "".join( map (lambda y: str(y) + " & ", x))[:-2] + "\\\\ \\hline\n", list))
+    table = table[:-7]
+    print (begin + table + end)
+    return begin + table + end
+
+def initializeLatexFile(filename, lst):
+    begin = "\documentclass{article}\n\\usepackage[utf8]{inputenc}\n"
+    inf = "\\title{python_hm_02}\n\\author{sheremeev.andrey}\n\date{February 2022}\n"
+    parameters = "\\usepackage{graphicx}\n\\graphicspath{ {./artifacts/} }\n\\DeclareGraphicsExtensions{.pdf,.png,.jpg}\n\n\\begin{document}\n\n"
+
+    string = "".join(map(lambda x: str(x), lst))
+    end = "\n\end{document}\n"
+
+    if not os.path.exists("artifacts"):
+        os.mkdir("artifacts")
+    f = open('artifacts/' + filename, 'w')
+
+    file = begin + inf + parameters + string + end
+    # print(file)
+    f.write(file)
+    f.close()
+
+def medium(lst, scale, picture):
+    table = easy(lst)
+    string = "\n\\includegraphics[scale=" + str(scale) + "]{" + picture + "}\n"
+    lst = [table, string]
+
+    generate_image.generate_image(fib.fib)
+
+    initializeLatexFile("my_tex.tex", lst)
+
+
+def main():
+    lst = makeList(5)
+    medium(lst, 0.2, "ast.png")
+
+if __name__ == '__main__':
+    main()
