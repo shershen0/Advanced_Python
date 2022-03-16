5012db67f8d20afa5dc7abb6c4cbf4b2c5d04b70
Andrey Sheremeev <sheremeev.andrey@gmail.com>
2022-03-16 22:19:10 +0300

    not u can give exect number of pictures and dir for them

--- hm_05/easy.py
+++ hm_05/easy.py
@@ -11,19 +11,22 @@ async def downloadPicture(url) -> bytes:
 
 async def writeToFile(el):
     pic = await downloadPicture(el[0])
-    if not os.path.exists("artifacts/easy"):
-        os.makedirs("artifacts/easy")
+    if not os.path.exists(el[2]):
+        os.makedirs(el[2])
     # lock when writing to file ?
-    f = await aiofiles.open("artifacts/easy/picture" + str(el[1]) + ".png", 'wb')
+    f = await aiofiles.open(el[2] + "/picture" + str(el[1]) + ".png", 'wb')
     await f.write(pic)
     await f.close()
 
 
 async def run():
+    count = input()
+    dirr = input()
     url = 'https://picsum.photos/200/300'
-    urls = [(url, i) for i in range(10)]
+    urls = [[url, i, dirr] for i in range(int(count))]
     await asyncio.wait(map(writeToFile, urls))
 
 
 if __name__ == '__main__':
     asyncio.run(run())
+
